cbcd1c130273db41e92ed6d2b6ee4cde76df9058
Ingo Reitz <9l@9lo.re>
2025-11-19 08:19:13 +0100

    linux v6.17.8

--- mkKernel.nix
+++ mkKernel.nix
@@ -4,10 +4,10 @@
   ...
 }:
 pkgs.linuxManualConfig rec {
-  version = "6.16.4";
+  version = "6.17.8";
   src = pkgs.fetchurl {
     url = "mirror://kernel/linux/kernel/v${pkgs.lib.versions.major version}.x/linux-${version}.tar.xz";
-    hash = "sha256-1qXjxxoQtTOnViUTh8yL9Iu9XHbYQrpelX2LHDFqtiI=";
+    hash = "sha256-Wo3mSnX8pwbAHGwKd891p0YYQ52xleJfHwJor2svsdo=";
   };
   inherit configfile;
   allowImportFromDerivation = true;
